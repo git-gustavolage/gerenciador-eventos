08a4fd77a6093caf1f756782dafec6323bf16d7d
git-gustavolage <lageg901@gmail.com>
2026-05-27 23:28:52 -0400

    feat: retorna o evento salvo na sessão do user

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -2,15 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
+
 class ManagerController extends Controller
 {
     public function view()
     {
-        return inertia("Manager/Index");
-    }
+        $event = $this->getCurrentEvent();
+
+        if (!$event) {
+            return redirect()->route("events.create");
+        }
+
+        return inertia("Manager/Index", [
+            "event" => $event,
+        ]);
+    }       
 
     public function general()
     {
-        return inertia("Manager/General/Index");
+        $event = $this->getCurrentEvent();
+
+        if (!$event) {
+            return redirect()->route("events.create");
+        }
+
+        return inertia("Manager/General/Index", [
+            "event" => $event,
+        ]);
+    }
+
+    private function latestEventCreatedBy(int $id_user): ?Evento
+    {
+        return Evento::query()->where("id_user", $id_user)->latest()->first();
+    }
+
+    private function getCurrentEvent(): ?Evento
+    {
+        $event_id = session("current_event_id");
+        if ($evento = Evento::query()->find($event_id)) {
+            return $evento;
+        }
+
+        $evento = $this->latestEventCreatedBy(auth("web")->id());
+
+        session(["current_event_id" => $evento->id]);
+
+        return $evento;
     }
 }
