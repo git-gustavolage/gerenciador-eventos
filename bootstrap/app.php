b2e260182f050459b2005c9b009c89a8af61da0f
git-gustavolage <lageg901@gmail.com>
2026-06-07 23:23:53 -0400

    feat: middleware para validacao de perfil admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,25 +2,32 @@
 
 use App\Exceptions\ApplicationException;
 use App\Exceptions\UserHasNoEventsException;
+use App\Http\Middleware\AssertAdmin;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__ . "/../routes/web.php", commands: __DIR__ . "/../routes/console.php", health: "/up")
+    ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(
             append: [
-                \App\Http\Middleware\HandleInertiaRequests::class,
-                \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+                HandleInertiaRequests::class,
+                AddLinkHeadersForPreloadedAssets::class,
             ],
         );
+
+        $middleware->alias([
+            'admin' => AssertAdmin::class,
+        ]);
 
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (UserHasNoEventsException $e) {
-            return redirect()->to(route("eventos.create"));
+            return redirect()->to(route('eventos.create'));
         });
 
         $exceptions->render(function (ApplicationException $e) {
