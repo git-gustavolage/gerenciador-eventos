74f163443f1f9e20ab37a175f25101bd68535128
gustavo <lageg901@gmail.com>
2026-06-11 16:02:09 -0400

    hotfix: tratamento de exceptions

--- app/Actions/Ministrante/StoreMinistranteAction.php
+++ app/Actions/Ministrante/StoreMinistranteAction.php
@@ -2,11 +2,13 @@
 
 namespace App\Actions\Ministrante;
 
+use App\Exceptions\CreationFailedException;
 use App\Models\Evento;
 use App\Models\Ministrante;
 use App\Models\User;
 use App\Support\CurrentEvent;
 use Illuminate\Support\Facades\Gate;
+use Throwable;
 
 class StoreMinistranteAction
 {
@@ -23,15 +25,19 @@ class StoreMinistranteAction
             $contaVinculada = User::query()->where('email', $data['email'])->first();
         }
 
-        Ministrante::create([
-            'id_user' => $id_user,
-            'conta_id' => $contaVinculada ? $contaVinculada->id : null,
-            'nome' => $data['nome'],
-            'email' => $data['email'] ?? null,
-            'telefone' => $data['telefone'] ?? null,
-            'cargo' => $data['cargo'] ?? null,
-            'instituicao' => $data['instituicao'] ?? null,
-        ]);
+        try {
+            Ministrante::create([
+                'id_user' => $id_user,
+                'conta_id' => $contaVinculada ? $contaVinculada->id : null,
+                'nome' => $data['nome'],
+                'email' => $data['email'] ?? null,
+                'telefone' => $data['telefone'] ?? null,
+                'cargo' => $data['cargo'] ?? null,
+                'instituicao' => $data['instituicao'] ?? null,
+            ]);
+        } catch (Throwable $e) {
+            throw new CreationFailedException('Ocorreu um erro ao cadastrar o novo ministrante. Tente novamente.');
+        }
     }
 
     private function validate(User $user, Evento $evento): void
