447ad1231b42b26ad79d54a48f19e23014e724a9
gustavo <lageg901@gmail.com>
2026-06-11 09:53:47 -0400

    fix: validacao para gerenciamento de ministrantes, ambientes e evento

--- app/Actions/Evento/StoreEventoAction.php
+++ app/Actions/Evento/StoreEventoAction.php
@@ -7,46 +7,59 @@ use App\Enum\PerfilEnum;
 use App\Exceptions\CreationFailedException;
 use App\Models\Evento;
 use App\Models\Organizador;
+use App\Models\User;
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\DB;
 
 class StoreEventoAction
 {
     public function execute(int $id_user, EventoData $input): Evento
     {
+        $user = User::query()->findOrFail($id_user);
+
+        $this->validate($user);
+
         try {
             return DB::transaction(function () use ($id_user, $input) {
                 $inicio = Carbon::now()->addMonthNoOverflow()->startOfMonth()->startOfDay();
                 $fim = Carbon::now()->addMonthNoOverflow()->endOfMonth()->endOfDay();
 
                 $evento = Evento::create([
-                    "titulo" => $input->titulo,
-                    "descricao" => $input->descricao,
-                    "formato" => $input->formato,
-                    "categorias" => $input->categorias,
-                    "id_user" => $id_user,
-                    "id_local"    => $input->id_local,
-                    "is_publicado" => false,
-                    "is_cancelado" => false,
-                    "is_encerrado" => false,
-                    "data_inicio" => $inicio,
-                    "data_fim" => $fim,
+                    'titulo' => $input->titulo,
+                    'descricao' => $input->descricao,
+                    'formato' => $input->formato,
+                    'categorias' => $input->categorias,
+                    'id_user' => $id_user,
+                    'id_local' => $input->id_local,
+                    'is_publicado' => false,
+                    'is_cancelado' => false,
+                    'is_encerrado' => false,
+                    'data_inicio' => $inicio,
+                    'data_fim' => $fim,
                 ]);
 
                 Organizador::create([
-                    "perfil" => PerfilEnum::GESTOR->value,
-                    "id_evento" => $evento->id,
-                    "id_user" => $id_user,
+                    'perfil' => PerfilEnum::GESTOR->value,
+                    'id_evento' => $evento->id,
+                    'id_user' => $id_user,
                 ]);
 
                 return $evento;
             });
         } catch (Exception $e) {
-            throw new CreationFailedException("Ocorreu um erro ao criar o evento.", [
-                "message" => $e->getMessage(),
-                "id_user" => $id_user,
+            throw new CreationFailedException('Ocorreu um erro ao criar o evento.', [
+                'message' => $e->getMessage(),
+                'id_user' => $id_user,
             ]);
+        }
+    }
+
+    private function validate(User $user): void
+    {
+        if ($user->admin) {
+            throw new AuthorizationException('Não autorizado.');
         }
     }
 }
--- app/Actions/Evento/UpdateEventoAction.php
+++ app/Actions/Evento/UpdateEventoAction.php
@@ -4,13 +4,11 @@ namespace App\Actions\Evento;
 
 use App\Enum\StoragePathEnum;
 use App\Exceptions\UpdateFailedException;
-use App\Models\Atividade;
 use App\Models\Evento;
 use App\Models\User;
 use App\Support\S3Manager;
 use App\Support\StoreImage;
 use Exception;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class UpdateEventoAction
@@ -24,59 +22,29 @@ class UpdateEventoAction
 
         $oldBanner = $evento->banner_path;
 
-        if ($banner = $input["banner"] ?? null) {
+        if ($banner = $input['banner'] ?? null) {
             try {
-                $input["banner_path"] = StoreImage::save(StoragePathEnum::BANNERS->value, $banner);
+                $input['banner_path'] = StoreImage::save(StoragePathEnum::BANNERS->value, $banner);
 
-                unset($input["banner"]);
+                unset($input['banner']);
             } catch (Exception $e) {
-                throw new UpdateFailedException("Ocorreu um erro ao salvar a nova imagem. Tente novamente.", [
-                    "message" => $e->getMessage(),
+                throw new UpdateFailedException('Ocorreu um erro ao salvar a nova imagem. Tente novamente.', [
+                    'message' => $e->getMessage(),
                 ]);
             }
         }
 
-        $estavaCancelado = $evento->is_cancelado;
+        $evento->update($input);
 
-        if (isset($input['is_cancelado']) && $input['is_cancelado'] && !$estavaCancelado) {
-
-            $userIdsEvento = $evento->inscricoesEvento()->pluck('id_user');
-            dump($userIdsEvento);
-
-            $userIdsAtividades = DB::table('inscricoes_atividades')
-                ->join('atividades', 'atividades.id', '=', 'inscricoes_atividades.id_atividade')
-                ->where('atividades.id_evento', $evento->id)
-                ->pluck('id_user');
-
-            $userIdsUnicos = $userIdsEvento->merge($userIdsAtividades)->unique();
-
-            if ($userIdsUnicos->isNotEmpty()) {
-                $inscritos = User::query()->whereIn('id', $userIdsUnicos)->get();
-                foreach ($inscritos as $inscrito) {
-                    Mail::to($inscrito->email)->send(new EventoCanceladoMail($eventoAtualizado, $inscrito));
-                }
-            }
-
-            // Pra apagar as inscrições
-            $evento->inscricoesEvento()->delete();
-
-            $atividades = Atividade::query()->where('id_evento', $evento->id)->get();
-
-            $evento->inscricoesEvento()->inscricoes()->delete();
-
-            $evento->update($input);
-
-            $evento->fresh();
-
-        if (isset($input["banner_path"]) && $oldBanner && $oldBanner !== $input["banner_path"]) {
+        if (isset($input['banner_path']) && $oldBanner && $oldBanner !== $input['banner_path']) {
             S3Manager::delete($oldBanner);
         }
 
         return $evento->fresh();
     }
 
-    private function validate(User $user, Evento $evento)
+    private function validate(User $user, Evento $evento): void
     {
-        Gate::forUser($user)->authorize("update", $evento);
+        Gate::forUser($user)->authorize('update', $evento);
     }
 }
