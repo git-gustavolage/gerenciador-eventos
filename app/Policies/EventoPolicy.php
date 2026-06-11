6feb0f27e625efc1dddb56a3907fe3dd82abb1b1
gustavo <lageg901@gmail.com>
2026-06-11 09:53:08 -0400

    chore: melhoria da autorizacao no ocntexto do evento

--- app/Policies/EventoPolicy.php
+++ app/Policies/EventoPolicy.php
@@ -17,21 +17,67 @@ class EventoPolicy
 
     public function show(User $user, Evento $evento): bool
     {
-        return $user->id === $evento->id_user || $evento->organizadores()->where("id_user", $user->id)->exists();
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
     }
 
     public function update(User $user, Evento $evento): bool
     {
-        return $user->id === $evento->id_user || $evento->organizadores()->where("id_user", $user->id)->exists();
+        if ($evento->is_cancelado) {
+            return false;
+        }
+
+        if ($evento->is_encerrado) {
+            return false;
+        }
+
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
     }
 
     public function inviteOrganizador(User $user, Evento $evento): bool
     {
-        return $user->id === $evento->id_user || $evento->organizadores()->where("id_user", $user->id)->exists();
+        if ($evento->is_cancelado) {
+            return false;
+        }
+
+        if ($evento->is_encerrado) {
+            return false;
+        }
+
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
     }
 
     public function createAtividade(User $user, Evento $evento): bool
     {
-        return $user->id === $evento->id_user || $evento->organizadores()->where("id_user", $user->id)->exists();
+        if ($evento->is_cancelado) {
+            return false;
+        }
+
+        if ($evento->is_encerrado) {
+            return false;
+        }
+
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
+    }
+
+    public function cancel(User $user, Evento $evento): bool
+    {
+        if ($evento->is_encerrado) {
+            return false;
+        }
+
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
+    }
+
+    public function publish(User $user, Evento $evento): bool
+    {
+        if ($evento->is_cancelado) {
+            return false;
+        }
+
+        if ($evento->is_encerrado) {
+            return false;
+        }
+
+        return $user->id === $evento->id_user || $evento->organizadores()->where('id_user', $user->id)->exists();
     }
 }
